9c2a9df98e9e14401b1524da51586d4be240c2c8
fksalans <fksalans@users.noreply.github.com>
2020-05-27 14:21:02 +0900

    3주차 과제 수정

--- homework/homework_3w.py
+++ homework/homework_3w.py
@@ -3,18 +3,18 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
-for page in range(5):
-    data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg='+str(page),headers=headers)
+for page in range(1, 5):
+    data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200526=N&pg='+str(page),headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
     songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-    
     for song in songs:
-        rank = song.select_one('td.number', {'td class':'number'}).parser('<span>').text.strip()
-        #title = song.select_one('td.info > a.title.ellipsis').text.strip()
-        #singer = song.select_one('td.info > a.artist.ellipsis').text.strip()
-        print(rank)
-
-                                        
+        rank_tags = song.select_one('td.number').contents
+        rank = None  #초기화
+        if rank_tags and len(rank_tags) is not None:
+            rank = rank_tags[0].strip() 
+            title = song.select_one('td.info > a.title.ellipsis').text.strip()
+            singer = song.select_one('td.info > a.artist.ellipsis').text.strip()
+            print(rank, title, singer)
                                     
 
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
